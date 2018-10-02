35820cb6279e67ac9579f1c85c33fd0006de20bb
Claire Duvallet <cduvallet@gmail.com>
2018-10-01 21:17:07 -0400

    Goyal 2018: metadata, explore, butyrate

---
+++ src/data/goyal2018.make_metadata.sh
@@ -0,0 +1,4 @@
+# To do: copy over the code that cleans up the Goyal 2018 metadata in here
+
+# For now, just run the notebook that does this...
+jupyter nbconvert --execute ../exploration/2018-10-01.goyal2018.metadata.ipynb
